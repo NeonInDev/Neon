ee4db248c2ae76ad49f8ab2f614f29ef20342de9
NeonInDev <Lavisteseutorres@outlook.com>
2026-08-19 20:31:57 -0300

    NeonAcordar: mantem o PC acordado ate o usuario chegar (evita re-sleep por idle) e sobe a Neon

--- tmp_check_shutdown.ps1
+++
@@ -1,6 +0,0 @@
-$evt = Get-WinEvent -FilterHashtable @{LogName='System'; Id=41} -MaxEvents 1 -ErrorAction SilentlyContinue
-if ($evt) {
-  Write-Output $evt.TimeCreated.ToString('dd/MM/yyyy HH:mm:ss')
-} else {
-  Write-Output 'none'
-}
